acc35bc8e60708f2a5a42305918bd8c1b3fda75e
Maxence <maxence@aircall.io>
2020-03-08 12:56:53 +0100

    🔧 add react-router-dom to navigate between pages

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Header } from './components/organisms/header/Header'
+import { TaskCreator } from './components/organisms/taskCreator/TaskCreator';
+import { Cards } from './components/organisms/cards/Cards';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/" component={TaskCreator} exact />
+              <Route path="/task/:id" component={Cards} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
