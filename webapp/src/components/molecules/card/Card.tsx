301c8338baba08c5ea8e94a1106906fefdc95eaa
Maxence <maxence@aircall.io>
2020-03-08 14:22:49 +0100

    ✨ draft possibility to submit a vote for a task

--- webapp/src/components/molecules/card/Card.tsx
+++ webapp/src/components/molecules/card/Card.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Card.css';
 
-export class Card extends React.Component<{}, {}> {
+export interface CardProps {
+  value: number;
+  onCardSelect(e: React.MouseEvent<HTMLButtonElement>, value: number): void;
+}
+
+export class Card extends React.Component<CardProps, {}> {
   render() {
-    return <div>Card molecule</div>;
+    return (
+      <div>
+        <button onClick={e => this.props.onCardSelect(e, this.props.value)}>
+          {this.props.value}
+        </button>
+      </div>
+    );
   }
 }
--- webapp/src/components/organisms/cards/Cards.tsx
+++ webapp/src/components/organisms/cards/Cards.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './Cards.css'
+import React from 'react';
+import { Card } from '../../molecules/card/Card';
+
+import './Cards.css';
 
 export class Cards extends React.Component<{}, {}> {
+  cardValues(): Array<number> {
+    return [0, 0.5, 1, 2, 3, 5, 8, 13];
+  }
+
+  onCardSelect(
+    event: React.MouseEvent<HTMLButtonElement>,
+    value: number
+  ): void {
+    console.log('on card select', value);
+  }
+
   render() {
-    return(
-      <header>
-        Cards component
-      </header>
-    )
+    return (
+      <div>
+        {this.cardValues().map((cardValue: number) => (
+          <div key={cardValue.toString()}>
+            <Card
+              value={cardValue}
+              onCardSelect={this.onCardSelect.bind(this)}
+            />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
