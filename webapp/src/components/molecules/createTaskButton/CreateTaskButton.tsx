21f1704568f84c41ed9bbf92153c1c43fbce47fe
Maxence <maxence@aircall.io>
2020-03-08 13:22:18 +0100

    ✨ add draft version to create new task and share it to others

--- webapp/src/components/molecules/createTaskButton/CreateTaskButton.tsx
+++ webapp/src/components/molecules/createTaskButton/CreateTaskButton.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './CreateTaskButton.css'
 
-export class CreateTaskButton extends React.Component<{}, {}> {
+export interface CreateTaskButtonProps {
+  onCreateTask(e: React.MouseEvent<HTMLButtonElement>): void,
+  isDisabled: boolean
+}
+
+export class CreateTaskButton extends React.Component<CreateTaskButtonProps, {}> {
   render() {
     return(
-      <header>
-        CreateTaskButton molecule
-      </header>
+      <button disabled={this.props.isDisabled} onClick={ (e) => this.props.onCreateTask(e) }>
+        Create a task
+      </button>
     )
   }
 }
--- webapp/src/components/organisms/taskCreator/TaskCreator.tsx
+++ webapp/src/components/organisms/taskCreator/TaskCreator.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import './TaskCreator.css'
+import { CreateTaskButton } from '../../molecules/createTaskButton/CreateTaskButton'
 
-export class TaskCreator extends React.Component<{}, {}> {
+export interface TaskCreatorState {
+  taskId: string;
+}
+
+export class TaskCreator extends React.Component<{}, TaskCreatorState> {
+  constructor() {
+    super({});
+
+    this.state = {
+      taskId: ''
+    };
+  }
+
+  onCreateTask(event: React.MouseEvent<HTMLButtonElement>): void {
+    console.log('on create task button click', event);
+
+    this.setState({ taskId: 'toto' });
+  }
+
+  isButtonDisabled(): boolean {
+    return this.state.taskId !== '';
+  }
+
   render() {
     return(
       <div>
-        TaskCreator organism
+        <CreateTaskButton onCreateTask={this.onCreateTask.bind(this)} isDisabled={this.isButtonDisabled()}/>
+        {
+          this.state.taskId !== ''
+          ? <div>
+            Your task has been created!<br/>
+            Share <a href={`http://localhost:4000/task/${this.state.taskId}`} target="_blank">this link</a> to your colleagues to vote
+          </div>
+          : null
+        }
       </div>
     )
   }
