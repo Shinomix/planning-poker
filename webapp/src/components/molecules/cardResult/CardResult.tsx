ffcee331a749c3adcfdbd899fc012b7e616c7391
Maxence <maxence@aircall.io>
2020-03-08 14:43:27 +0100

    🚧 draft a way to display results of the vote using state update

--- webapp/src/components/molecules/cardResult/CardResult.tsx
+++ webapp/src/components/molecules/cardResult/CardResult.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './CardResult.css';
 
-export class CardResult extends React.Component<{}, {}> {
+export interface CardResultProps {
+  count: number;
+}
+
+export class CardResult extends React.Component<CardResultProps, {}> {
   render() {
-    return <div>CardResult molecule</div>;
+    const pluralizedVote = this.props.count > 1 ? 'votes' : 'vote';
+    return (
+      <div>
+        {this.props.count} {pluralizedVote}
+      </div>
+    );
   }
 }
--- webapp/src/components/organisms/cards/Cards.tsx
+++ webapp/src/components/organisms/cards/Cards.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { Card } from '../../molecules/card/Card';
 
 import './Cards.css';
+import { CardResult } from '../../molecules/cardResult/CardResult';
 
-export class Cards extends React.Component<{}, {}> {
+export interface CardsState {
+  results: Map<number, number>;
+}
+
+export class Cards extends React.Component<{}, CardsState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      results: new Map(),
+    };
+  }
+
   cardValues(): Array<number> {
     return [0, 0.5, 1, 2, 3, 5, 8, 13];
   }
@@ -13,6 +26,18 @@ export class Cards extends React.Component<{}, {}> {
     value: number
   ): void {
     console.log('on card select', value);
+
+    const currentValue: number = this.state.results
+      ? this.state.results.get(value) || 0
+      : 0;
+
+    this.setState({
+      results: this.state.results.set(value, currentValue + 1),
+    });
+  }
+
+  resultFor(value: number) {
+    return this.state.results ? this.state.results.get(value) || 0 : 0;
   }
 
   render() {
@@ -24,6 +49,7 @@ export class Cards extends React.Component<{}, {}> {
               value={cardValue}
               onCardSelect={this.onCardSelect.bind(this)}
             />
+            <CardResult count={this.resultFor(cardValue)} />
           </div>
         ))}
       </div>
